94def33cd569200ef712026a544601a29f6fd317
tujunlan <tujunlan2011@gmail.com>
2019-04-16 09:22:21 +0800

    create

---
+++ src/main/java/goodthings/GoodthingsApplication.java
@@ -0,0 +1,13 @@
+package goodthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodthingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodthingsApplication.class, args);
+    }
+
+}
