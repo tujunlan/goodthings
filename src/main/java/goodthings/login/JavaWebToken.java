51d92df23a21b9a19070e6e34750f80b6606b8e3
tujunlan <tujunlan2011@gmail.com>
2019-05-10 08:35:00 +0800

    8th upd

---
+++ src/main/java/goodthings/login/JavaWebToken.java
@@ -0,0 +1,37 @@
+package goodthings.login;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Map;
+
+public class JavaWebToken {
+
+    private static Key getKeyInstance() {
+//        return MacProvider.generateKey();
+        //We will sign our JavaWebToken with our ApiKey secret
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("APP");
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        return signingKey;
+    }
+
+    public static String createJavaWebToken(Map<String, Object> claims) {
+        return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS256, getKeyInstance()).compact();
+    }
+
+    public static Map<String, Object> verifyJavaWebToken(String jwt) {
+        try {
+
+            Map<String, Object> jwtClaims =
+                    Jwts.parser().setSigningKey(getKeyInstance()).parseClaimsJws(jwt).getBody();
+            return jwtClaims;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- web/src/store/modules/user.js
+++ web/src/store/modules/user.js
@@ -5,7 +5,8 @@ import { resetRouter } from '@/router'
 const state = {
   token: getToken(),
   name: '',
-  avatar: ''
+  avatar: '',
+  uid:''
 }
 
 const mutations = {
@@ -17,6 +18,9 @@ const mutations = {
   },
   SET_AVATAR: (state, avatar) => {
     state.avatar = avatar
+  },
+  SET_UID: (state, uid) => {
+    state.uid = uid
   }
 }
 
@@ -46,10 +50,11 @@ const actions = {
           reject('Verification failed, please Login again.')
         }
 
-        const { name, avatar } = data
+        const { name, avatar,uid } = data
 
         commit('SET_NAME', name)
         commit('SET_AVATAR', avatar)
+        commit('SET_UID', uid)
         resolve(data)
       }).catch(error => {
         reject(error)
